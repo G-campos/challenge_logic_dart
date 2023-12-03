221eaf40507b98ae6ead3bf6f6200dd3d303671d
Gabriel de Campos <gabodecampos@gmail.com>
2023-12-02 21:09:27 -0300

    init commit

---
+++ bin/challenge_logic_dart.dart
@@ -0,0 +1,5 @@
+import 'package:challenge_logic_dart/challenge_logic_dart.dart' as challenge_logic_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${challenge_logic_dart.calculate()}!');
+}
---
+++ test/challenge_logic_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:challenge_logic_dart/challenge_logic_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
